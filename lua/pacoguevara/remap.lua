6be263df5d7ef3aded79dfe6ce316598307ead3a
Paco Guevara <pacoguevaraa@gmail.com>
2023-01-24 01:04:10 -0600

    Add remappings

--- lua/pacoguevara/remap.lua
+++ lua/pacoguevara/remap.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "ww", ":w<CR>")
+vim.keymap.set("n", "qq", ":q<CR>")
+
+-- Clear search highlight
+vim.keymap.set("n", "<esc>", ":noh<return><esc>")
+
